12115643d8bd67a729e8b959054b311ac7397c30
krule14 <43622545+krule14@users.noreply.github.com>
2021-01-19 15:07:53 -0800

    First Check-in
    Created project, created (unbacked) UI, created class library, created class ImageScrape, created unit test (passes).

---
+++ PowerpointProject/ImageSearchLibrary/ImageScrape.cs
@@ -0,0 +1,90 @@
+﻿/// Copyright © 2021, Katelyn Rule 
+/// License: GPL3
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace PowerpointProject
+{
+
+    /// There exists 3rd party libraries that you can use, 
+    /// but the free ones are broken.
+    /// This is free, but is not guaranteed to work forever,
+    /// but it worked when I uploaded it. 
+
+    public class ImageSearch
+    {
+        /// <summary>
+        /// Simple request formatter.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static string FormatMultipleSearchEntries(string key)
+        {
+            string ret = "";
+            bool bLastWhite = false;
+            foreach (var c in key.Trim())
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    if (!bLastWhite)
+                        ret += "+";
+                    bLastWhite = true;
+                }
+                else
+                {
+                    ret += c;
+                    bLastWhite = false;
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Cheezy Bing Image Search Screen scraper
+        /// </summary>
+        /// <param name="key">single word key for search</param>
+        /// <returns></returns>
+        public static List<string> Search(string key, string suffix = "jpg")
+        {
+            try
+            {
+                key = FormatMultipleSearchEntries(key);
+                var urls = new List<string>();
+
+                // Make Request
+                var queryURL =
+                    "https://www.bing.com/images/search?q=" + key + "&qs=n&pq=" + key +
+                    "&sc=8-5&first=1&tsc=ImageBasicHover";
+
+                var request = (HttpWebRequest)HttpWebRequest.Create(queryURL);
+
+                // Get Response
+                var response = (HttpWebResponse)request.GetResponse();
+                var htmlSource = new StreamReader(response.GetResponseStream());
+                var r = htmlSource.ReadToEnd();
+
+                // We don't care about parsing this mess. All we want is some
+                // jpg links that came back. Returned clickable image responses are in hrefs.
+                // So we will find them and strip out the URLs and return them.
+                foreach (Match match in Regex.Matches(r, "href=\"([^\"]*)\""))
+                {
+                    var url = match.Groups[1].Value;
+                    if (url.Contains(suffix) && url.Substring(url.Length - 3) == suffix)
+                    {
+                        urls.Add(url);
+                    }
+                }
+
+                return urls;
+            }
+            catch (Exception)
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
---
+++ PowerpointProject/ImageSearchLibraryTests/ImageSearchTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerpointProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerpointProject.Tests
+{
+    [TestClass()]
+    public class ImageSearchTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.IsTrue(ImageSearch.Search("Light Box").Count > 0);
+        }
+    }
+}
